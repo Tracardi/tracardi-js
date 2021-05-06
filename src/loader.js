3f3904fe2d3b6b63fcd28f1bf5acf6dc00621726
risto <risto.kowaczewski@gmail.com>
2021-05-07 01:31:38 +0200

    init

--- src/loader.js
+++ src/loader.js
@@ -1,64 +1,64 @@
 window.tracker || (window.tracker = {});
-            (function () {
+(function () {
 
-                var trackerQueue = [];
+    const trackerQueue = [];
 
-                var methods = ['track', 'page', 'identify'];
+    const methods = ['track', 'page', 'identify'];
 
-                var factory = function (method) {
-                    return function () {
-                        let args = Array.prototype.slice.call(arguments);
-                        args.unshift(method);
-                        trackerQueue.push(args);
-                        return window.tracker;
-                    };
-                };
+    const factory = function (method) {
+        return function () {
+            let args = Array.prototype.slice.call(arguments);
+            args.unshift(method);
+            trackerQueue.push(args);
+            return window.tracker;
+        };
+    };
 
-                // For each of our methods, generate a queueing stub.
-                for (var i = 0; i < methods.length; i++) {
-                    var method = methods[i];
-                    window.tracker[method] = factory(method);
+    // For each of our methods, generate a queueing stub.
+    for (var i = 0; i < methods.length; i++) {
+        var method = methods[i];
+        window.tracker[method] = factory(method);
+    }
+
+    function callback(e) {
+        console.debug("Rerun callbacks")
+        // Now window.tracardi.default is presnet
+        window.tracker = window.tracardi.default
+        while (trackerQueue.length > 0) {
+            const item = trackerQueue.shift();
+            const method = item.shift();
+            if (tracker[method]) {
+                tracker[method].apply(tracker, item);
+            }
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        let script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        if (options.url !== null) {
+            script.src = options.url + '/dist/tracker.js';
+        } else {
+            script.src = 'dist/tracker.js';
+        }
+
+
+        if (script.addEventListener) {
+            script.addEventListener('load', function (e) {
+                if (typeof callback === 'function') {
+                    callback(e);
                 }
-
-                function callback(e) {
-                    console.debug("Rerun callbacks")
-                    // Now window.tracardi.default is presnet
-                    window.tracker = window.tracardi.default
-                    while (trackerQueue.length > 0) {
-                        const item = trackerQueue.shift();
-                        const method = item.shift();
-                        if (tracker[method]) {
-                            tracker[method].apply(tracker, item);
-                        }
-                    }
+            }, false);
+        } else {
+            script.onreadystatechange = function () {
+                if (this.readyState === 'complete' || this.readyState === 'loaded') {
+                    callback(window.event);
                 }
+            };
+        }
 
-                document.addEventListener('DOMContentLoaded', function() {
-                    let script = document.createElement('script');
-                    script.type = 'text/javascript';
-                    script.async = true;
-                    if(options.url !== null) {
-                        script.src = options.url + '/dist/tracker.js';
-                    } else {
-                        script.src = 'dist/tracker.js';
-                    }
-
-
-                    if (script.addEventListener) {
-                        script.addEventListener('load', function (e) {
-                            if (typeof callback === 'function') {
-                                callback(e);
-                            }
-                        }, false);
-                    } else {
-                        script.onreadystatechange = function () {
-                            if (this.readyState === 'complete' || this.readyState === 'loaded') {
-                                callback(window.event);
-                            }
-                        };
-                    }
-
-                    let first = document.getElementsByTagName('script')[0];
-                    first.parentNode.insertBefore(script, first);
-                    });
-        })();
+        let first = document.getElementsByTagName('script')[0];
+        first.parentNode.insertBefore(script, first);
+    });
+})();
